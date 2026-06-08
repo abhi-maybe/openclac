fb04dc16748b790ff1327cdd1e7f94dd0c52fb33
Abhi <abhi@users.noreply.github.com>
2026-06-09 00:24:51 +0530

    fix: add BrowserRouter basename for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/openclac">
       <App />
     </BrowserRouter>
   </React.StrictMode>
